1781442f966227d388497e2efc8dc9afbd7e9ac2
Matt M <65567181+matthewmanzi@users.noreply.github.com>
2020-07-07 23:22:06 -0400

    update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
+//Task is to create a clean repsonsive interface that generates a random password on-click, with prompts beforehand asking the user whether they would like to satisfy the 0following conditions:
 
+//Must be between 8 and 128 characters
+//Prompt for including lowercase 
+//Prompt whether to include numbers 
+//Prompt for using uppercase 
+//Prompt for using special characters 
+//Sanity check for user input
+//GUI level display output
+
+
+
+
+funciton pwLength() //between 8 and 128 characters??
+
+function pwLowercase()
+
+function pwUppercase()
+
+pwNum = [1,2,3,4,5,6,7,8,9,0]
